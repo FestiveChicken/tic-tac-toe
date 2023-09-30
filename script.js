3ba459f0b19fa2dc2e7b932717009773dfea43c8
FestiveChicken <44416957+FestiveChicken@users.noreply.github.com>
2023-09-30 17:32:42 -0400

    Created blank gameboard

--- script.js
+++ script.js
@@ -1,40 +1,74 @@
 const gameboard = (() => {
-
   //selects gameboardholder div in html
   const gameboardHolder = () => {
     return document.getElementById('gameboardHolder')
   }
+
+  //counts the number of squares
   const numberOfSquares = () => {
     return document.querySelectorAll('.square').length
   }
 
-  //creates board
+  //creates square
   const createSquare = () => {
     const gameboardSquare = document.createElement('div')
-    gameboardSquare.classList.add('square');
-    gameboardSquare.style.backgroundColor = 'black'
-    gameboardSquare.style.padding = '50px'
+    gameboardSquare.setAttribute('class', 'square');
+    gameboardSquare.style.border = 'solid'
+    gameboardSquare.style.borderColor = 'black'
+    gameboardSquare.style.width = '50px'
+    gameboardSquare.style.height = '50px'
     return gameboardHolder().appendChild(gameboardSquare)
   }
 
+  //creates board and assigns each square an id that will correlate to the array
   const createBoard = () => {
-      while (numberOfSquares < 9) {
-        createSquare()
-      return createSquare ()
+    i = 0
+    while (numberOfSquares() < 9) {
+      createSquare()
+      const squares = document.querySelectorAll('.square')
+      const lastSquare = squares[i]
+      lastSquare.setAttribute('id', i)
+      i++
       }
-      }
+      return
+    }
 
   return {
-    createSquare, createBoard
+    createBoard
   }
 })()
 
-const createPlayer = () => {
-    const playerOne = ''
-    const playerTwo = ''
+
+
+const player = () => {
+  const squares = querySelectorAll('.square')
+
+  const getID = () => {
+    squares.forEach(square => {
+      square.addEventListener('click', () => {
+      console.log(square.id) })
+    })
+    console.log(getID)
+    console.log('fuck me')
+}
+return {
+  getID
+}
 }
 
 const gameFlow = () => {
 }
 
+gameboard.createBoard()
 
+const squares = document.querySelectorAll('.square')
+
+
+const wtf = squares.forEach(square => {
+    square.addEventListener('click', () => {
+    return square.id
+    })
+  })
+
+  console.log(wtf)
+  
