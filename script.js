c9aa6d6d78638e162bb1e30a23bbfca9a4321d4c
Uesli Keta <44416957+FestiveChicken@users.noreply.github.com>
2023-10-04 15:36:26 -0400

    Added turns and check so array does not change if value is already there

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const gameboard = (() => {
+  j = 0
+  gameboardArray = new Array(9)
+  
+
   //selects gameboardholder div in html
   const gameboardHolder = () => {
     return document.getElementById('gameboardHolder')
@@ -9,16 +13,44 @@ const gameboard = (() => {
     return document.querySelectorAll('.square').length
   }
 
+
   //creates square
   const createSquare = () => {
     const gameboardSquare = document.createElement('div')
     gameboardSquare.setAttribute('class', 'square');
-    gameboardSquare.style.border = 'solid'
-    gameboardSquare.style.borderColor = 'black'
-    gameboardSquare.style.width = '50px'
-    gameboardSquare.style.height = '50px'
-    return gameboardHolder().appendChild(gameboardSquare)
-  }
+    if (gameboardArray[j] == 'x') {
+      gameboardSquare.style.background = "url('images/cross.svg')"
+      gameboardSquare.style.backgroundSize = 'contain'
+      gameboardSquare.style.border = 'solid'
+      gameboardSquare.style.borderWidth = '2px'
+      gameboardSquare.style.borderColor = 'black'
+      gameboardSquare.style.width = 'auto'
+      gameboardSquare.style.height = 'auto'
+      j++
+      return gameboardHolder().appendChild(gameboardSquare)
+    }
+    else if (gameboardArray[j] == 'o') {
+      gameboardSquare.style.background = "url('images/circle.svg')"
+      gameboardSquare.style.backgroundSize = 'contain'
+      gameboardSquare.style.backgroundColor = 'white'
+      gameboardSquare.style.border = 'solid'
+      gameboardSquare.style.borderWidth = '2px'
+      gameboardSquare.style.borderColor = 'black'
+      gameboardSquare.style.width = 'auto'
+      gameboardSquare.style.height = 'auto'
+      j++
+      return gameboardHolder().appendChild(gameboardSquare)}
+    else {
+      gameboardSquare.style.backgroundColor = 'green'
+      gameboardSquare.style.border = 'solid'
+      gameboardSquare.style.borderWidth = '2px'
+      gameboardSquare.style.borderColor = 'black'
+      gameboardSquare.style.width = 'auto'
+      gameboardSquare.style.height = 'auto'
+      j++
+      return gameboardHolder().appendChild(gameboardSquare)
+    }
+    }
 
   //creates board and assigns each square an id that will correlate to the array
   const createBoard = () => {
@@ -33,42 +65,115 @@ const gameboard = (() => {
       return
     }
 
+  const changeArray = () => {
+    while (numberOfSquares() != 0) {
+      const square = document.querySelector('.square')
+      gameboardHolder().removeChild(square)
+      j = 0
+    }
+    return
+  }
+
+  const clearBoard = () => {
+    while (numberOfSquares() != 0) {
+      const square = document.querySelector('.square')
+      gameboardHolder().removeChild(square)
+      j = 0
+      gameboardArray = new Array(9)
+    }
+    return
+  }
+
+  createBoard()
+
   return {
-    createBoard
+    createBoard, gameboardArray, changeArray, clearBoard
   }
 })()
 
 
 
-const player = () => {
-  const squares = querySelectorAll('.square')
+const player = (moveNumber) => {
+  const move = () => moveNumber
 
-  const getID = () => {
-    squares.forEach(square => {
-      square.addEventListener('click', () => {
-      console.log(square.id) })
-    })
-    console.log(getID)
-    console.log('fuck me')
-}
 return {
-  getID
-}
-}
-
-const gameFlow = () => {
+  move
+  }
 }
 
-gameboard.createBoard()
+const winOrTie = (() => {
+  const rowOne = gameboardArray[0] + gameboardArray[1] + gameboardArray[2]
+  const winner = () => {
+  if (rowOne == ['x','x','x']) {
+    console.log('player one is winner')
+    gameboard.clearBoard()
+    gameboard.createBoard()
+  }
+  if (gameboardArray[0] == 'o' && gameboardArray[1] == 'o' && gameboardArray[2] == 'o') {
+    console.log('player two is winner')
+    gameboard.clearBoard()
+    gameboard.createBoard()
+  }
+  else return
+  }
+  return {
+    winner, rowOne
+  }
+})()
 
-const squares = document.querySelectorAll('.square')
 
-
-const wtf = squares.forEach(square => {
-    square.addEventListener('click', () => {
-    return square.id
-    })
-  })
-
-  console.log(wtf)
+const gameFlow = (() => {
+  //initilizing variable
+  let squareID = null
   
+  //getting id of each square
+  const getSquareID = (event) => {
+    const gameboardHolder = document.getElementById('gameboardHolder')
+    if(gameboardHolder.contains(event.target)) {
+      const square = event.target
+      squareID = square.id
+      if (squareID == 'gameboardHolder') {
+        return
+      }
+      turn()
+    }
+  }
+
+  document.addEventListener('click',getSquareID)
+
+
+
+  const turn = () => {
+    if (gameboardArray[squareID] == null) {
+      if (playerOne.move() == playerTwo.move()) {
+        gameboard.changeArray()
+        gameboardArray[squareID] = 'x'
+        gameboard.createBoard()
+        playerOne = player(playerOne.move() + 1)
+        winOrTie.winner()
+        rows
+      }
+      else {
+        gameboard.changeArray()
+        gameboardArray[squareID] = 'o'
+        gameboard.createBoard()
+        playerTwo = player(playerTwo.move() + 1)
+        winOrTie.winner()
+      }
+  }
+  else {
+    return  
+  }
+
+  }
+  return
+})()
+
+const rows = (() => {
+  let rowOne = gameboardArray[0] + gameboardArray[1] + gameboardArray[2]
+  return rowOne
+})
+
+
+let playerOne = player(1)
+let playerTwo = player(1)
